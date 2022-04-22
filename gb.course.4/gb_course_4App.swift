85937021de31842750ee41fc1f61272123194394
Valera <r3volution1@bk.ru>
2022-04-22 13:15:21 +0300

    l01
    swiftyui. login view

---
+++ gb.course.4/gb_course_4App.swift
@@ -0,0 +1,17 @@
+//
+//  gb_course_4App.swift
+//  gb.course.4
+//
+//  Created by yoji on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct gb_course_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
